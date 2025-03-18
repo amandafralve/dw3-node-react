73d0199b7912711b059da1d18b27d6d920c5d8ce
amandafralve <amanda.fralve@gmail.com>
2025-03-18 20:09:42 -0300

    add: finalizando crud

--- api-the-games/controllers/gameController.js
+++ api-the-games/controllers/gameController.js
@@ -1,8 +1,10 @@
-import gameServices from "../services/gameServices.js";
+import Game from "../models/Games.js";
+import gameService from "../services/gameServices.js";
+import { ObjectId, ObjectIds } from "mongodb";
 
 const getAllGames = async (req, res) => {
     try{
-        const games = await gameServices.getAll();
+        const games = await gameService.getAll();
         res.status(200).json({games:games})
     }catch(error){
         console.log(error)
@@ -10,4 +12,70 @@ const getAllGames = async (req, res) => {
     }
 }
 
-export default { getAllGames }
+// Cadastrando um jogo
+const createGame = async (req,res) => {
+    try{
+        const {title, plataform, year, price} = req.body
+        await gameService.Create(title, plataform, year, price)
+        res.sendStatus(201) // Código CREATED
+
+    }catch(error){
+        console.log(error)
+        res.status(500).json({ error:"Erro interno no servidor."})
+    }
+}
+
+// Apagando um jogo
+const deleteGame = async (req,res) => {
+    try{
+        if(ObjectId.isValid(req.params.id)){
+            const id = req.params.id
+            gameService.Delete(id)
+            res.sendStatus(204)
+        }else{
+            res.sendStatus(400)
+        }
+    }catch(error){
+        console.log(error)
+        res.status(500).json({ error: "Erro interno no servidor. "})
+    }
+}
+
+// Alterando um jogo
+const updateGame = async(req, res) => {
+    try{
+        if(ObjectId.isValid(req.params.id)){
+            const id = req.params.id;
+            const {title, plataform, year, price} = req.body
+            const game = gameService.Update(title, plataform, year, price)
+            res.status(200).json({ game })
+        }else{
+            res.sendStatus(400); // Bad Request 400
+        }
+    }catch(error){
+        console.log(error);
+        res.sendStatus(500) // Erro interno no servidor
+    }
+}
+
+// Listar um jogo
+const getOneGame = async(req,res)=>{
+    try{
+        if(ObjectId.isValid(req,params.id)){
+            const id = req.params.id
+            const game = await gameService.getOne(id);
+            if(!game){
+                res.sendStatus(404) // jogo não encontrado
+            }else{
+                res.send(200).json({ game})
+            }
+        }else{
+            res.sendStatus(400) // Bad Request
+        }
+    }catch(error){
+        console.log(error)
+        res.sendStatus(500)
+    }
+}
+
+export default { getAllGames, createGame, deleteGame, getOneGame }
--- api-the-games/routes/gameRoutes.js
+++ api-the-games/routes/gameRoutes.js
@@ -4,5 +4,13 @@ import gameController from "../controllers/gameController.js";
 
 // Endpoint para listar todos os jogos
 gameRoutes.get("/games", gameController.getAllGames)
+// Endpoint para cadastrar um jogo
+gameRoutes.post("/game", gameController.createGame)
+// Endpoint para apagar um jogo
+gameRoutes.delete("/games/:id", gameController.deleteGame)
+// Endpoint alterar dado jogo
+gameRoutes.put("/game/:id", gameController.updatedGame)
+// Endpoint listar registro único
+gameRoutes.get("/game/:id", gameController.getOneGame)
 
 export default gameRoutes;
